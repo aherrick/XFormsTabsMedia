36edb695e99b45723371ce0885e8b57f3bab1107
Andrew Herrick <aherrick@indiana.edu>
2017-09-06 12:32:09 -0400

    check camera permission

--- XFormsTabsMedia/XFormsTabsMedia/TabPage1.xaml.cs
+++ XFormsTabsMedia/XFormsTabsMedia/TabPage1.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Plugin.Media;
 using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,29 +32,44 @@ namespace XFormsTabsMedia
 
             if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
             {
-                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                await Application.Current.MainPage.DisplayAlert("No Camera", "No camera available.", "OK");
                 return;
             }
 
-            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            var cameraStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+            if (cameraStatus != PermissionStatus.Granted)
             {
-                PhotoSize = PhotoSize.Large,
-                AllowCropping = true,
-                Directory = "Sample",
-                Name = "test.jpg"
-            });
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Camera });
+                cameraStatus = results[Permission.Camera];
+            }
 
-            if (file == null)
-                return;
-
-            await DisplayAlert("File Location", file.Path, "OK");
-
-            var imgSource = ImageSource.FromStream(() =>
+            if (cameraStatus == PermissionStatus.Granted)
             {
-                var stream = file.GetStream();
-                file.Dispose();
-                return stream;
-            });
+                MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    PhotoSize = PhotoSize.Large,
+                    AllowCropping = true,
+                    Name = "cellar-beer.jpg"
+                });
+
+                if (file != null)
+                {
+                    // post image to server!
+                }
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Permissions Denied", "Unable to take photos.", "OK");
+
+                if (Device.RuntimePlatform == Device.iOS)
+                {
+                    var result = await Application.Current.MainPage.DisplayAlert("Settings", "Update camera permissions?", "Yes", "No");
+                    if (result)
+                    {
+                        CrossPermissions.Current.OpenAppSettings();
+                    }
+                }
+            }
         }
     }
 }
